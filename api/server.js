607c8bc564e2960c566ed4a24d0b75f126614ddb
Ogden-R <rebeccaogden97@gmail.com>
2022-03-04 14:31:19 -0600

    api being built

--- api/server.js
+++ api/server.js
@@ -1,64 +1,40 @@
-const express = require('express')
-const server = express()
-const cors = require('cors')
+const express = require('express');
+const server = express();
+const cors = require('cors');
 
-server.use(express.json())
-server.use(cors())
-
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => [
     res.send('Hello from server!')
-])
+]);
 
-const users = [
-    {
-        user: 'marymanne',
-        password: 'thisisstupid1'
-    },
-    {
-        user: 'iambatman',
-        password: 'notsuperman'
-    },
-    {
-        user: 'teachersrock73',
-        password: 'loveteaching'
-    },
-    {
-        user: 'david9387',
-        password: 'RMSweirdo'
-    }
-]
+server.get('/wizards', (req, res) => {
+    const wizards = [
+      {
+        id: 1,
+        name: 'Harry Potter',
+      },
+      {
+        id: 2,
+        name: 'Ronald Weasley',
+      },
+      {
+        id: 3,
+        name: 'Hermoine Granger',
+      },
+      {
+        id: 4,
+        name: 'Draco Malfoy',
+      },
+    ];
+  
+    res.status(200).json(wizards);
+  });
 
-const register = [
-    {
-        user: 'partygirl',
-        password: 'blingbling'
-    }
-]
 
-const login = [
-    {
-        user: 'partygirl',
-        password: 'blingbbling'
-    }
-]
+server.get('/api/wizards', (req,res) => {
+    res.status(200).json(wizards)
+});
 
-server.get('/api/users', (req,res) => {
-    res.status(200).json(users)
-})
-server.post('/api/register', (req, res) => {
-  res.status(201).json(register)
-})
-server.post('api/login', (req, res) => {
-   if(!login){
-      res.status(404).json({
-          message: 'Please enter correct credentials'
-      })
-   }else{
-       res.status(200).json({
-           message: 'Welcome'
-       })
-   }
-})
-
-module.exports = server 
+module.exports = server;
